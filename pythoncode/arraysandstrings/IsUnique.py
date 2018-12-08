4799b96aec69ec670d26906abd77c993841e3da5
vevinkirk <virkkevin15@gmail.com>
2018-12-08 14:23:00 -0800

    arrayandstrings added to both python and java

---
+++ pythoncode/arraysandstrings/IsUnique.py
@@ -0,0 +1,36 @@
+# O(N)
+import unittest
+
+
+def isUnique(string):
+    # Assuming character set is ASCII (128 characters)
+    if (string == None or len(string) > 256):
+        return False
+
+    char_set = [False for _ in range(256)]
+    for char in string:
+        val = ord(char)
+        if char_set[val]:
+            # Char already found in string
+            return False
+        char_set[val] = True
+
+    return True
+
+
+class Test(unittest.TestCase):
+    dataT = [('abcd'), ('s4fad'), ('')]
+    dataF = [('23ds2'), ('hb 627jh=j ()')]
+
+    def test_unique(self):
+        # true check
+        for test_string in self.dataT:
+            actual = isUnique(test_string)
+            self.assertTrue(actual)
+        # false check
+        for test_string in self.dataF:
+            actual = isUnique(test_string)
+            self.assertFalse(actual)
+
+if __name__ == "__main__":
+    unittest.main()
