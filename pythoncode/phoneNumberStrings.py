7b537f194842018e0c8d08a3617660a69b3458d8
Kevin Virk <13743689+vevinkirk@users.noreply.github.com>
2018-12-18 11:27:50 -0800

    Create phoneNumberStrings.py

---
+++ pythoncode/phoneNumberStrings.py
@@ -0,0 +1,22 @@
+digit_map = {
+    '1':' ',
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+    '0': "+",
+}
+
+def word_numbers(input):
+  input = str(input)
+  ret = ['']
+  for char in input:
+    letters = digit_map.get(char, '')
+    ret = [anything+letter for anything in ret for letter in letters]
+  return ret
+ 
+print(word_numbers("31234"))
