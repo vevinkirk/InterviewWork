9b26c0e915087086f9e9af2bcbd0d6d079ef7cac
vevinkirk <virkkevin15@gmail.com>
2019-01-14 15:24:18 -0800

    updated syntax

--- pythoncode/updateBoard.py
+++ pythoncode/updateBoard.py
@@ -12,16 +12,16 @@ def update(board):
     markerArray = []
 
     totalBoard = len(board)
-innerArrays = len(board[0])
+    innerArrays = len(board[0])
     for x in range(totalBoard):
         for j in range(innerArrays):
             if board[x][j] >= 1:
                 counter = board[x][j]
             if board[x][j+1] = counter && board[x+1][j] &&   board[x+1][j+1]:
-markerArray.append((x,j))
-markerArray.append((x,j+1))
-markerArray.append((x+1,j))
-markerArray.append((x+1,j+1))
+                markerArray.append((x,j))
+                markerArray.append((x,j+1))
+                markerArray.append((x+1,j))
+                markerArray.append((x+1,j+1))
 
     for k in range(len(markerArray)):
         x = markerArray[k]
@@ -30,9 +30,9 @@ markerArray.append((x+1,j+1))
         board[xCoord][yCoord] = 0
         while(xCoord != 0):
             board[xCoord][yCoord] = board[xCoord-1][yCoord]
-board[xCoord-1][yCoord] = 0
-xCoord = xCoord - 1
-        board[0][yCoord] = 0
+            board[xCoord-1][yCoord] = 0
+            xCoord = xCoord - 1
+            board[0][yCoord] = 0
     if len(markerArray) =  0:
         exit
     update(board)
